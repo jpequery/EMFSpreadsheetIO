b504b83f67a6602d1ed2ce385deeaf6acf0dea3b
jpequery <jerome.pequery@obeo.fr>
2017-06-30 22:07:28 +0200

    adding copyright in all java files

--- plugins/org.obeonetwork.spreadsheet.exporter/src/org/obeonetwork/spreadsheet/exporter/extensions/ExcelExporterMetamodelExtensionDescriptor.java
+++ plugins/org.obeonetwork.spreadsheet.exporter/src/org/obeonetwork/spreadsheet/exporter/extensions/ExcelExporterMetamodelExtensionDescriptor.java
@@ -1,4 +1,14 @@
-package org.obeonetwork.spreadsheet.exporter.extensions;
+/*******************************************************************************
+ *  Copyright (c) 2016 Obeo. 
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *   
+ *   Contributors:
+ *       Obeo - initial API and implementation
+ *  
+ *******************************************************************************/package org.obeonetwork.spreadsheet.exporter.extensions;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
